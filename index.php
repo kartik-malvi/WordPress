e9f93a15915eef5b59b8d656eb9fadce46771e95
kartik-malvi <kartikmalviya.oo9@gmail.com>
2023-05-03 18:01:54 +0530

    first commit 5section

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>" >
+   
+   <?php wp_head(); ?>
+</head>
+<body <?php body_class('babayaga'); ?>>
+    <P>hello</p>
+
+
+    <?php wp_footer(); ?>
+</body>
+</html>
